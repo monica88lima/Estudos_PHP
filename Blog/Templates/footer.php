11bc0f79de732aad237dc0da98a734ab7bf353c6
monica88lima <flima.monica@gmail.com>
2023-08-26 16:47:30 -0300

    Inclusão de footer e header

---
+++ Blog/Templates/footer.php
@@ -0,0 +1,22 @@
+<?php
+   include_once("helpers/url.php");
+
+?>
+
+
+<footer>
+<figure>
+        <a href="https://github.com/monica88lima" target="_blank">
+          <img src="<?= $BASE_URL ?>/img/github.png" alt=" git" /></a>
+
+        <a href="https://www.linkedin.com/in/monicalima/" target="_blank">
+          <img src="<?= $BASE_URL ?>/img/linkedin.png"alt="linkedin" /></a>
+
+        <a href="tel:952183958">
+          <img src="<?= $BASE_URL ?>/img/zap.png"alt="telefone" /></a>
+      </figure>
+
+</footer>
+
+</body>
+</html>
--- Blog/contato.php
+++ Blog/contato.php
@@ -2,7 +2,7 @@
  include_once("templates/header.php");
 ?>
 
-    <h1>Olá fdsagfsfgh gfhdht </h1>
+    <h1>Pagina de Contato </h1>
     
 <?php
  include_once("templates/footer.php");
