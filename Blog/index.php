f15f10ee1b26bcb3368b131f639aa06ed4156853
monica88lima <flima.monica@gmail.com>
2023-08-26 16:16:18 -0300

    Blog arquivos de configuração inicial

---
+++ Blog/index.php
@@ -0,0 +1,22 @@
+<?php
+   include_once("helpers/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Mônica Codando</title>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <link rel="icon" href="<?= $BASE_URL ?>/img/fav.ico" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <h1>Olá fdsagfsfgh gfhdht </h1>
+</body>
+</html>
